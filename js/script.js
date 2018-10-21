0da5723cfaec1cadd9595a461671ebc389f53e55
Jasmine Smith <jasmine.smith28@outlook.com>
2018-10-21 16:32:44 +0100

    Added fonts

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log("Hello Worlds")
+$(function () {
+
+var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+url += '?' + $.param({
+  'api-key': "82f12f3e4189492f86578cb2d4e6add0"
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(data) {
+  console.log(data.results[0].abstract);
+  const abstract = data.results[0].abstract;
+  console.log(data.results[0].multimedia[0]);
+  const image = data.results[0].multimedia[0];
+  console.log(data.results[0].url);
+  const url = data.results[0].url;
+}).fail(function(err) {
+  throw err;
+});
+
+
+
+});
