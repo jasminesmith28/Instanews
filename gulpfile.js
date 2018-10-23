a8a27231f5d650c54e66e93f451f5dc6c549ef2c
Jasmine Smith <jasmine.smith28@outlook.com>
2018-10-23 10:14:29 +0100

    ajax link

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,29 @@
-const gulp = require('gulp'),
+  const gulp = require('gulp'),
   prettyError = require('gulp-prettyerror'),
   sass = require('gulp-sass'),
   autoprefixer = require('gulp-autoprefixer'),
   rename = require('gulp-rename'),
   cssnano = require('gulp-cssnano'),
-  uglify = require('gulp-uglify'),
-  eslint = require('gulp-eslint');
-  
-  var browserSync = require('browser-sync').create();
+  uglify = require('gulp-uglify-es').default,
+  eslint = require('gulp-eslint'),
+  browserSync = require('browser-sync');
 
 // Create basic Gulp tasks
 
-gulp.task('sass', function(done) {
-  gulp
-    .src('./sass/style.scss', { sourcemaps: true })
-    .pipe(prettyError())
+gulp.task("sass", function() {
+  return gulp
+    .src("./sass/style.scss")
+    .pipe(prettyError()) // ADD THIS LINE
     .pipe(sass())
     .pipe(
       autoprefixer({
-        browsers: ['last 2 versions']
+        browsers: ["last 2 versions"]
       })
     )
-    .pipe(gulp.dest('./build/css'))
+    .pipe(gulp.dest("./build/css"))
     .pipe(cssnano())
-    .pipe(rename('style.min.css'))
-    .pipe(gulp.dest('./build/css'));
-
-  
-  done();
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("./build/css"));
 });
 
 gulp.task('lint', function() {
--- js/script.js
+++ js/script.js
@@ -4,24 +4,24 @@ var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
 url += '?' + $.param({
   'api-key': "82f12f3e4189492f86578cb2d4e6add0"
 });
+
 $.ajax({
   url: url,
   method: 'GET',
 }).done(function(data) {
-  console.log(data.results[0].abstract);
-  const abstract = data.results[0].abstract;
-  console.log(data.results[0].multimedia[0]);
-  // const image = data.results[0].multimedia[0];
-  console.log(data.results[0].url);
-  // const url = data.results[0].url;
-}).done(function(data) {
-  $()
+  $('.abstract').empty();
+  for(var i=0; i<12; i++){
+  var abstract = data.results[0].abstract;
+  console.log(abstract);
+  var image = data.results[0].multimedia[4].url;
+  console.log(image);
+  var url = data.results[0].url;
+  console.log(url);
+  
+  $('.abstract').append('<a href="'+url+'"class="link"><img src="'+image+'" class="articleimage"><h1 class="description">'+abstract+'</h2></a>');
 }
 
 }).fail(function(err) {
   throw err;
 });
-
-
-
 });
