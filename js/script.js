2c018dbc79b6b53cfe0e22083c9274182b54ebc7
Jasmine Smith <jasmine.smith28@outlook.com>
2018-10-23 15:01:55 +0100

    animations

--- js/script.js
+++ js/script.js
@@ -1,27 +1,34 @@
-$(function () {
+  
+  $('select').on('change', function(){
+    var section = $('select').val();
+    var url = "https://api.nytimes.com/svc/topstories/v2/"+section +".json";
+    url += '?' + $.param({
+        'api-key': "82f12f3e4189492f86578cb2d4e6add0"
+    });
 
-var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
-url += '?' + $.param({
-  'api-key': "82f12f3e4189492f86578cb2d4e6add0"
-});
+$('header').toggleClass("reduce");
+$('body').toggleClass("shift");
 
 $.ajax({
   url: url,
   method: 'GET',
 }).done(function(data) {
-  $('.abstract').empty();
+  $('.article_section').empty();
   for(var i=0; i<12; i++){
-  var abstract = data.results[0].abstract;
+  var abstract = data.results[i].abstract;
   console.log(abstract);
-  var image = data.results[0].multimedia[4].url;
+  var image = data.results[i].multimedia[4].url;
   console.log(image);
-  var url = data.results[0].url;
+  var url = data.results[i].url;
   console.log(url);
   
-  $('.articleone').append('<section><a href="'+url+'"class="link"><img src="'+image+'" class="articleimage"><h2 class="description">'+abstract+'</h2></a></section>');
+  $('.article_section').append('<section><a href="'+url+'"class="link"><img src="'+image+'" class="articleimage"></img><h2 class="description">'+abstract+'</h2></a></section>');
 }
 
 }).fail(function(err) {
   throw err;
 });
-});
+
+{/* <style>background-image:url("'+image+'") class="articleimage"</style> */}
+})
+
